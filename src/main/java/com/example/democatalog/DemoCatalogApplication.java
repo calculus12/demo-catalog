60d020ca1a5a8822af076557d088d9c97386b07d
kimnamju <kimnamju0920@gmail.com>
2025-12-14 16:26:03 +0900

    add flyway starter dependency

--- src/main/java/com/example/democatalog/DemoCatalogApplication.java
+++ src/main/java/com/example/democatalog/DemoCatalogApplication.java
@@ -1,10 +1,8 @@
 package com.example.democatalog;
 
-import com.example.democatalog.config.PolarProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
