965455ba70047c47df85abb0df526ef73706e4b8
kimnamju <kimnamju0920@gmail.com>
2025-12-14 13:42:43 +0900

    load mockito agent statically

--- src/test/java/com/example/democatalog/DemoCatalogApplicationTests.java
+++ src/test/java/com/example/democatalog/DemoCatalogApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class DemoCatalogApplicationTests {
 
     @Autowired
